28835d1c3d7e5ab579b6e4217958411e2651d44a
Caleb VanDerMaas <calebvandermaasc@gmail.com>
2024-08-18 14:31:52 -0400

    fix

--- examples/phat/test.py
+++ examples/phat/test.py
@@ -7,7 +7,7 @@ from update_display import draw_text
 
 for attempt in range(1, 61):
     try:
-        gps_data = fetch_gps_data
+        gps_data = fetch_gps_data()
     except Exception as e:
         print(f'attempt #{attempt} failed: {e}')
         if attempt < retries:
@@ -19,4 +19,4 @@ for attempt in range(1, 61):
 if gps_data: 
     lat, lon, time = gps_data
     output_string = f'lat: {lat}, lon: {lon}, time: {time}'
-    draw_text(output_string)
+    draw_text(output_string, (2,2))
